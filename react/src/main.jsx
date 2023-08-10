b8f01903c371f939f106fb5af77671ea695a0bea
Hrishikesh Venkatesh <hv122@ic.ac.uk>
2023-08-10 15:07:27 +0100

    - organised the react components

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './login.jsx'
-import Register from './register.jsx'
+import App from './components/login.jsx'
+import Register from './components/register.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Register />
   </React.StrictMode>,
 )
