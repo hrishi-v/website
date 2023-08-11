c6cfc58e80caefc3d4d6aa3fa808a6b68d2a62a0
Hrishikesh Venkatesh <hv122@ic.ac.uk>
2023-08-11 19:07:08 +0100

    - router working with results and landing page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Results from "./pages/Results";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Landing />} />
+          <Route path="/results" element={<Results />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ react/src/pages/Results.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Results() {
+  return (
+    <>
+      <Header />
+      <h2>Results</h2>
+    </>
+  );
+}
