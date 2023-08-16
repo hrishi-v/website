4aeaeac8dba1737ee0738f611ad6d38efe8fc3a2
Hrishikesh Venkatesh <hv122@ic.ac.uk>
2023-08-16 21:17:37 +0100

    - CatalogueListing almost complete - added assets for products, shop front close to working

--- react/src/components/CatalogueListing.jsx
+++ react/src/components/CatalogueListing.jsx
@@ -11,36 +11,41 @@ HTML DONE
 add a function to take parameters, and change price, title, and catalogue images
 */
 import { Button } from "react-bootstrap";
-import { useState } from "react";
 import { ShopContext } from "../shop-context";
+import React, { useContext } from "react";
 
-export default function CatalogueListing() {
+export const CatalogueListing = () => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
   return (
     <>
-      <div className="product-img-div">
-        <img
-          className="responsive"
-          src="https://images.asos-media.com/products/nike-all-over-logo-bandana-in-white/204134625-1-white?$n_640w$&wid=513&fit=constrain"
-        ></img>
+      <div className="product">
+        <img className="responsive" src={productImage}></img>
         <div className="options">
           <Button bsPrefix="del-btn">
             <img
               className="del-img"
               src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"
             />
+            <div className="description">
+              <p>
+                <b>{productName}</b>
+              </p>
+              <p>${price}</p>
+            </div>
           </Button>
-          <Button bsPrefix="add-btn">
+          <button className="addToCartBttn" onClick={() => addToCart(id)}>
             <img
               className="addimg"
               src="https://icons.veryicon.com/png/o/miscellaneous/o2o-middle-school-project/plus-104.png"
-            />
-          </Button>
+            />{" "}
+            {cartItemCount > 0 && <>({cartItemCount})</>}
+          </button>
         </div>
-      </div>
-      <div className="product-info">
-        <h5 className="product-name">Nike all over logo bandana</h5>
-        <h6 className="price">$25.00</h6>
       </div>
     </>
   );
-}
+};
--- react/src/pages/Shop/Shop.jsx
+++ react/src/pages/Shop/Shop.jsx
@@ -1,12 +1,23 @@
 import UniversalNavBar from "../../components/UniversalNavBar";
 import React from "react";
 import CatalogueListing from "../../components/CatalogueListing";
+import { PRODUCTS } from "../../products";
 
-export default function Shop() {
+export const Shop = () => {
   return (
     <>
       <UniversalNavBar />
-      <h1>Hello</h1>
+      <div className="shop">
+        <div className="shopTitle">
+          <h1>Shop</h1>
+        </div>
+
+        <div className="products">
+          {PRODUCTS.map((product) => (
+            <CatalogueListing data={product} />
+          ))}
+        </div>
+      </div>
     </>
   );
-}
+};
